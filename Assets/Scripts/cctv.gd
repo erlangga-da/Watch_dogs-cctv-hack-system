c95e5e573500d30bd6a5c99677a83d295200ab37
erlangga-da <erlanggamuhammad94@gmail.com>
2021-08-05 22:16:07 +0700

    cctv hacking system

--- Assets/Scripts/cctv.gd
+++ Assets/Scripts/cctv.gd
@@ -3,27 +3,32 @@ extends KinematicBody
 onready var head = $Cam
 onready var camera = $Cam/Camera
 onready var rayCast = $Cam/Camera/RayCast
-onready var Casthide = $Cam/RayCast
-onready var next_cctv = $"../Container/cctv2"
-onready var cctv_3 = $"../Container/cctv3"
-onready var cctv_view = $"../Container/cctv2/MeshInstance"
-onready var cctv_node = $"../Container"
+onready var Casthide = $Cam/Casthide
+onready var cctv_container = $"../Container/"
+onready var castedNode
+onready var savedNode
 
+var direction
+var travel = false
+var destination = Vector3()
+var mouse_sens = 0.02
 var zoom = 70
 var speed = 25
-var mouse_sens = 0.02
-var direction
-var destination = Vector3()
-var terbang = false
+
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	destination = self.translation
 	self.rotation_degrees.y = 0
 
+
 func _input(event):
+	#if 'E' pressed 
 	if event.is_action_pressed("interact"):
-		destination = next_cctv.translation
+		#check if castedNode have a translation value
+		if castedNode:
+			destination = castedNode.translation
+	
 	#zoom
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_WHEEL_UP:
@@ -35,7 +40,8 @@ func _input(event):
 			if camera.fov < 70:
 				camera.fov += 1
 	
-	if event is InputEventMouseMotion and !terbang:
+	#camera rotation
+	if event is InputEventMouseMotion and !travel:
 		head.rotate_y(deg2rad(-event.relative.x * mouse_sens))
 		#camera.rotate_x(deg2rad(event.relative.y * mouse_sens))
 		
@@ -45,28 +51,37 @@ func _input(event):
 		var head_rotation = head.rotation_degrees
 		head_rotation.y = clamp(head_rotation.y, -70, 70)
 		head.rotation_degrees = head_rotation
-	
+
+
 func _process(delta):
 	if Casthide.is_colliding():
-		#print(Casthide.get_collider().get_name())
-		if Casthide.get_collider().get_name() == "cctv2":
-			print(next_cctv.rotation_degrees)
-			self.rotation_degrees = next_cctv.rotation_degrees
-			cctv_node.remove_child(next_cctv)
-		if Casthide.get_collider().get_name() == "cctv3":
-			self.rotation_degrees = cctv_3.rotation_degrees
-			cctv_node.remove_child(next_cctv)
+		#set camera rotation when colliding
+		self.rotation_degrees = castedNode.rotation_degrees
 	
 	if rayCast.is_colliding():
-		if rayCast.get_collider().get_name() == "cctv3":
-			next_cctv = cctv_3
-			destination = next_cctv.translation
+		#get staticBody name
+		castedNode = cctv_container.get_node(rayCast.get_collider().get_name())
+		
+		#staticBody from Container node?? yes? = this is a cctv node , no? = not cctv
+		if castedNode.get_parent().get_name() == "Container":
+			castedNode.translation
+			#print("this node from Container")
 
 func _physics_process(delta):
+	#when the distance > 0.5 the camera is moving
 	if destination.distance_to(transform.origin) > 0.5:
-		terbang = true
+		travel = true
 		direction = destination - transform.origin
 		direction = direction.normalized() * speed
 		move_and_slide(direction)
 	else:
-		terbang = false
+		travel = false
+
+#hide cctv when entered area
+func _on_HideRange_body_entered(body):
+	savedNode = castedNode
+	savedNode.visible = false
+
+#show cctv when exit area
+func _on_HideRange_body_exited(body):
+	savedNode.visible = true
